4524793c6a0e89f7c29d2cf3fd82230e0b4f408b
Davi Santos <daviir17@gmail.com>
2023-04-30 16:53:36 -0300

    style(head de noticias): finalização do estilo do header de notícias

---
+++ src/components/buttonMenu/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+import Menu from '@mui/icons-material/Menu';
+
+export default function ButtonMenu({color, background}) {
+  return (
+    <button style={{color: `${color}`, backgroundColor: `${background}`}} className='botao'>
+      <Menu style={{height: "15px", width: "15px", marginRight: "15px", color: `${color}`}}/>
+      Menu
+    </button>
+  )
+}
--- src/components/headerNews/index.jsx
+++ src/components/headerNews/index.jsx
@@ -3,18 +3,35 @@ import Logo from '../logo';
 import ButtonHeader from '../buttonHeader';
 import './index.css';
 import whiteLogo from './whiteLogo.png';
+import ButtonMenu from '../buttonMenu';
+import usuario from './usuario.png'
 
 export default function HeaderNews() {
   return (
     <div className='container-news'>
         <div className='container-background'>
             <div className='content-news'>
-                <Logo color={'white'} logo={whiteLogo}/>
-                <ButtonHeader color={'white'} background={'transparent'}/>
+                <div className='botao-menu'>
+                    <ButtonMenu color={'white'} background={'transparent'}/>
+                </div>
+                <div className='logo'>
+                    <Logo color={'white'} logo={whiteLogo}/>
+                </div>
+                <div className='botao-entrar'>
+                    <ButtonHeader color={'white'} background={'transparent'}/>
+                </div>
+            </div>
+            <div className='burnout'>
+                Burnout: você sabe lidar com o <br/>esgotamento profissional?
             </div>
         </div>
         <div className='gradient'>
-                sobreposição
+                
+        </div>
+        <div className='usuario'>
+            <img src={usuario} alt="usuario" />
+            <p className='nome'>John Doe</p>
+            <p className='duracao'>Tempo de leitura: 10 minutos</p>
         </div>
     </div>
   )
