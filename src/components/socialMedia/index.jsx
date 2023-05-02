5b71907333d5f2259425b9479a44d02875226299
Davi Santos <daviir17@gmail.com>
2023-05-02 13:24:36 -0300

    feat(social-media): criando a componente das  redes sociais

---
+++ src/components/socialMedia/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import './index.css';
+
+export default function SocialMedia() {
+  return (
+    <div className='container-social'>
+      <InstagramIcon/>
+      <LinkedInIcon/>
+      <YouTubeIcon/>
+      <ChatBubbleIcon/>
+    </div>
+  )
+}
