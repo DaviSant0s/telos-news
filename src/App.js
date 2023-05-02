15b23d86df8614836423126ac27fd650fd64214c
Davi Santos <daviir17@gmail.com>
2023-05-02 20:07:58 -0300

    feat(footer): implementação do footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import Footer from './components/footer';
+import Header from './components/header';
 import HeaderNews from './components/headerNews';
+import Home from './pages/home';
 import PostNews from './pages/postNews';
 
 function App() {
   return (
     <div className="App">
-      <HeaderNews/>
-      <PostNews/>
+      <HeaderNews/> 
+      <PostNews/> 
+      {/* <Header/>
+      <Home/> */}
+      <Footer/>
     </div>
   );
 }
